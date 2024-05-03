9a58cdd28101c60fc3f97db9607ad030825cf7e8
Riley Rockwell <rileyrockwell@gmail.com>
2024-05-03 22:58:15 +0000

    updates

--- ps1/house_hunting.py
+++ ps1/house_hunting.py
@@ -1,3 +1,23 @@
 print("House Hunting...on the Western Seaboard.")
 
+
+annual_salary = input("Enter your annual salary: ")
+portion_saved = input("Enter the percent of your salary to save, as a decimal: ")
 total_cost = input("Enter the cost of your dream home: ")
+
+portion_down_payment = 0.25
+current_savings = 0
+
+# annual return: 0.04
+r = 0.04
+
+portion_saved = 0.51
+
+"""
+at the end of each month, your savings increase by the return on your investment, plus a percentage of your (monthly_salary) * (annual_salary / 12)
+"""
+
+def calculate_months(annual_salary, portion_saved, total_cost):
+    return -1
+
+print(calculate_months(annual_salary, portion_saved, total_cost))
