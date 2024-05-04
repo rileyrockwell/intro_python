06a9b21e1d4d08d8aa131a0a6814b8f235f731b9
Riley Rockwell <rileyrockwell@gmail.com>
2024-05-04 13:39:23 +0000

    updates

--- ps1/house_hunting.py
+++ ps1/house_hunting.py
@@ -1,9 +1,6 @@
-print("House Hunting...on the Western Seaboard.")
-
-
-annual_salary = input("Enter your annual salary: ")
-portion_saved = input("Enter the percent of your salary to save, as a decimal: ")
-total_cost = input("Enter the cost of your dream home: ")
+annual_salary = int(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = int(input("Enter the cost of your dream home: "))
 
 portion_down_payment = 0.25
 current_savings = 0
@@ -11,13 +8,16 @@ current_savings = 0
 # annual return: 0.04
 r = 0.04
 
-portion_saved = 0.51
-
 """
 at the end of each month, your savings increase by the return on your investment, plus a percentage of your (monthly_salary) * (annual_salary / 12)
 """
 
-def calculate_months(annual_salary, portion_saved, total_cost):
-    return -1
+monthly_salary = annual_salary / 12
+total_months = 0
 
-print(calculate_months(annual_salary, portion_saved, total_cost))
+while current_savings < portion_down_payment * total_cost:
+    current_savings += monthly_salary * portion_saved
+    current_savings += current_savings * (r/12)
+    total_months += 1
+
+print("Number of months: " + str(total_months))
